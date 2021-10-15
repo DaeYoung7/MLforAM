c59b01b4fb4fb284b60f436bf6a505c9d0fa9312
DaeYoung7 <djeodud7@naver.com>
2021-10-15 15:54:06 +0900

    modify p52

--- p52.py
+++ p52.py
@@ -1,7 +1,29 @@
 import numpy as np
 
+def getPCA(matrix):
+    eVal, eVec = np.linalg.eigh(matrix)
+    indices = eVal.argsort()[::-1]
+    eVal, eVec = eVal[indices], eVec[:,indices]
+    eVal = np.diagflat(eVal)
+    return eVal, eVec
+
 def getRndCov(nCols, nFacts):
     w = np.random.normal(size=(nCols, nFacts))
     cov = np.dot(w, w.T)
-    cov += np.diag(np.random.uniform(size=nCols))
+    cov += np.diag(np.random.uniform(size=nCols))  # full rank matrix로 변경
     return cov
+
+def cov2corr(cov):
+    std = np.sqrt(np.diag(cov))
+    print(cov.shape)
+    print(std.shape)
+    print(np.outer(std, std).shape)
+    corr = cov / np.outer(std, std)
+    corr[corr<-1], corr[corr>1] = -1, 1
+    return corr
+
+alpha, nCols, nFact, q = .995, 1000, 100, 10
+cov = np.cov(np.random.normal(size=(nCols*q, nCols)), rowvar=0)  # nCols, nCols 형태가 됨
+cov = alpha * cov + (1 - alpha) * getRndCov(nCols, nFact)
+corr0 = cov2corr(cov)
+eVal0, eVec0 = getPCA(corr0)  # 상관계수에서 고유값과 고유벡터구함..
